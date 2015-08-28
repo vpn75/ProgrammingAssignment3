377cb40987cf951c0509d3f9f8b6bac8a19eb036
Vamsi Nath <vpnath@gmail.com>
2015-08-28 16:17:18 -0400

    Initial checkin of script for part1
    Script to return best hospital by outcome for a specific state.

---
+++ best.R
@@ -0,0 +1,36 @@
+
+best <- function(state, outcome) {
+  #Define good outcomes
+  good <- c("heart attack","pneumonia","heart failure")
+  #Exit if outcome parameter not valid
+  if (!outcome %in% good) {
+    stop("invalid outcome")
+  }
+  #Get appropriate Outcome column index so we don't have to deal with long colnames
+  if (outcome == "heart attack") {
+    colidx <- 11
+  }
+  else if (outcome == "pneumonia") {
+    colidx <- 23
+  }
+  else {
+    colidx <- 35
+  }
+  
+  #Read in outcome data
+  data <- read.csv("outcome-of-care-measures.csv",stringsAsFactor = FALSE, 
+                   na.strings = "Not Available")[c(2,7,colidx)]
+  
+  #Perform state input validation
+  if (!state %in% data[,"State"]) {
+    stop("invalid state")
+  }
+  
+  sub <- data[data$State == state,]
+  sub <- na.omit(sub)
+  low <- min(sub[,3])
+  #Return name of hospital with lowest mortality rate
+  return(sub[sub[,3] == low,1])
+  
+}
+
